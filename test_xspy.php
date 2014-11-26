68570dac2c4fc362cf8dae23cede031aeaf68341
wangrenjun <wangrj1981@gmail.com>
2014-11-26 13:49:15 +0800

    update test
    a little change

--- test_xspy.php
+++ test_xspy.php
@@ -1,16 +1,12 @@
 <?php
-
 xspy_init("/usr/local/xunsearch/etc/py.xdb");
-
-$py = xspy_hanzi_to_pinyin("你好", 0, 0);
-echo $py . '\r\n';
-
-$py = xspy_hanzi_to_pinyin("你好", 1, 0); // 拼音模糊
-echo $py . '\r\n';
-
-$py = xspy_hanzi_to_pinyin("你好", 0, 1); // 拼音首字母缩写
-echo $py . '\r\n';
-
+$py = xspy_hanzi_to_pinyin("你好世界", 0, 0);
+echo $py . "\r\n";
+$py = xspy_hanzi_to_pinyin("你好世界", 1, 0); // 拼音模糊
+echo $py . "\r\n";
+$py = xspy_hanzi_to_pinyin("你好世界", 0, 1); // 拼音首字母缩写
+echo $py . "\r\n";
+$py = xspy_hanzi_to_pinyin("你好世界", 1, 1);
+echo $py . "\r\n";
 xspy_deinit();
-
 ?>
